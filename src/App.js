09f226347ef07d46652f4d543c7d18dbeefeae28
PetterBakke <74598862+PetterBakke@users.noreply.github.com>
2024-07-23 19:52:37 +0200

    Added the sass files to the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./sass/styles.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
