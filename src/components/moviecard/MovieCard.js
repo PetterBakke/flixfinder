36e61d0fc992ebf1a5533b13e1b4adb8cd295897
PetterBakke <74598862+PetterBakke@users.noreply.github.com>
2024-07-23 20:07:42 +0200

    Added new folder moviecard and added code to the moviecard.js

---
+++ src/components/moviecard/MovieCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './MovieCard.scss';
+
+const MovieCard = ({ title }) => {
+  return (
+    <div className="card">
+      <img src="path/to/movie-poster.jpg" alt={title} />
+      <div className="card-content">
+        <h3 className="title">{title}</h3>
+        <p className="description">Short description of the movie or series.</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
