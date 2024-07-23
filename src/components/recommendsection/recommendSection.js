683148551c1a1bd848d80f62c5d507b205a1b31b
PetterBakke <74598862+PetterBakke@users.noreply.github.com>
2024-07-23 20:06:34 +0200

    Added code to the recommendedsections.js

---
+++ src/components/recommendsection/recommendSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './RecommendedSection.scss';
+import MovieCard from './MovieCard';
+
+const RecommendedSection = ({ title }) => {
+  return (
+    <section className="recommended-section">
+      <h2 className="section-title">{title}</h2>
+      <div className="card-carousel">
+        {/* Example Movie Cards */}
+        <MovieCard title="Movie 1" />
+        <MovieCard title="Movie 2" />
+        <MovieCard title="Movie 3" />
+        {/* Add more MovieCard components as needed */}
+      </div>
+    </section>
+  );
+};
+
+export default RecommendedSection;
